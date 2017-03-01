07ff306d06690244c15580c18ad971f917d26a9b
ZillahPaolaPoliquit <poliquitzillahit1b@gmail.com>
2017-03-01 20:44:32 +0800

    yoko na beh'

--- public/js/calculator.js
+++ public/js/calculator.js
@@ -1,10 +1,10 @@
 function clickMeBEH() {
-	document.getElementByid('first').innerHTML = "Ching";
+	document.getElementById('first').innerHTML = "Ching";
 	}
 
 function addme() {
-	var x = document.getElementByid('fn').value;
-	var y = document.getElementByid('sn').value;
+	var x = document.getElementById('fn').value;
+	var y = document.getElementById('sn').value;
 	var sum = parseInt(x) + parseInt(y);
 	document.getElementByid('answer').innerHTML = sum;
 	}
